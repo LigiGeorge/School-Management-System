d68e10721df0b5a64bdf58a70234e0b2bf34c1c1
Ligi George <ligigeorge0310@gmail.com>
2023-09-27 14:54:59 +0530

    Reset Link,Admin(Add,Edit and Delete),Form validation unique emailaddress database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -87,6 +87,35 @@ class AuthController extends Controller
         }
 
     }
+    public function reset($remember_token)
+    {
+        $user=User::getTokenSingle($remember_token);
+        if(!empty($user))
+        {
+            $data['user']=$user;
+            return view('auth.reset',$data);
+        }
+        else
+        {
+            abort(404);
+        }
+    }
+    public function PostReset($token,Request $request)
+    {
+        if($request->password == $request->cpassword)
+        {
+            $user=User::getTokenSingle($token);
+            $user->password = Hash::make($request->password);
+            $user->remember_token=Str::random(30);
+            $user->save();
+            return redirect(url(''))->with('success',"Password successfully reset. ");
+        }
+        else
+        {
+            return redirect()->back()->with('error',"Password and confirm password doesn't match");
+        }
+
+    }
     public function logout()
     {
         Auth::logout();
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,13 +23,18 @@ Route::post('login',[AuthController::class,'AuthLogin']);
 Route::get('logout',[AuthController::class,'logout']);
 Route::get('forgot-password',[AuthController::class,'forgotpassword']);
 Route::post('forgot-password',[AuthController::class,'PostForgotPassword']);
+Route::get('reset/{token}',[AuthController::class,'reset']);
+Route::post('reset/{token}',[AuthController::class,'PostReset']);
 
-Route::get('admin/admin/list', function () {
-    return view('admin.admin.list');
-});
 
 Route::group(['middleware'=>'admin'],function(){
-    Route::get('admin/dashboard',[DashboardController::class,'dashboard']);    
+    Route::get('admin/dashboard',[DashboardController::class,'dashboard']); 
+    Route::get('admin/admin/list',[AdminController::class,'list']); 
+    Route::get('admin/admin/add',[AdminController::class,'add']);
+    Route::post('admin/admin/add',[AdminController::class,'insert']);
+    Route::get('admin/admin/edit/{id}',[AdminController::class,'edit']);
+    Route::post('admin/admin/edit/{id}',[AdminController::class,'update']);
+    Route::get('admin/admin/delete/{id}',[AdminController::class,'delete']);    
 });
 Route::group(['middleware'=>'teacher'],function(){
     Route::get('teacher/dashboard',[DashboardController::class,'dashboard']);
