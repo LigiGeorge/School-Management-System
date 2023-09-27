78585cd1accabf5cd7c5c5b8e9a102e67a892eca
Ligi George <ligigeorge0310@gmail.com>
2023-09-27 16:04:32 +0530

    Pagination and form fliter admin list

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Request;
 
 class User extends Authenticatable
 {
@@ -48,11 +49,24 @@ class User extends Authenticatable
     }
     static public function getAdmin()
     {
-        return User::select('users.*')
+        $return = User::select('users.*')
                           ->where('user_type','=',1)
-                          ->where('is_delete','=',0)
-                          ->orderBy('id','desc')
-                          ->get();
+                          ->where('is_delete','=',0);
+                          if(!empty(Request::get('name')))
+                          {
+                            $return=$return->where('name','like','%'. Request::get('name').'%');
+                          }
+                          if(!empty(Request::get('email')))
+                          {
+                            $return=$return->where('email','like','%'. Request::get('email').'%');
+                          }
+                          if(!empty(Request::get('date')))
+                          {
+                            $return=$return->whereDate('created_at','=',Request::get('date'));
+                          }
+        $return=$return->orderBy('id','desc')
+                       ->paginate(1);
+        return $return;
     }
     static public function getEmailSingle($email)
     {
